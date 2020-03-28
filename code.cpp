047d3e45f8cfa1b96b4ada79fd4cff15e97ba8eb
Prasoonagrawal <46186214+Prasoonagrawal@users.noreply.github.com>
2020-03-28 15:41:17 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -4,14 +4,14 @@ using namespace std;
 struct Process_Data
 {
 	int Num;
-	int Pid;  //Process Id
-	int A_time; //Process Arrival Time
 	int B_time; //Process Bruest Time
 	int Priority; //Process Priority
 	int F_time; //Process Finish Time
 	int R_time; //Process Remaining  Time During Execution
 	int W_time; //Waiting Time
 	int S_time; //Process start Time
+	int Pid;  //Process Id
+	int A_time; //Process Arrival Time
 	int Res_time;
 
 };
